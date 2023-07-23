a3988cdc814d47e7bf50161e9182c41f1af15258
Yuki Okamoto <yuki.okamoto@Yukis-MacBook-Pro-2.local>
2023-07-23 17:13:11 +0900

    test

---
+++ instant-meet-api/src/main/java/com/geek/scheduler/controller/CreateScheduleController.java
@@ -0,0 +1,15 @@
+package com.geek.scheduler.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1.1")
+public class CreateScheduleController {
+
+    @GetMapping("test")
+    public String testAPI() {
+        return "success";
+    }
+}
